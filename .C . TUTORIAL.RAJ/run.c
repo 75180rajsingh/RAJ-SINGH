04ee016399f365592d0f55ab066be2fae8fa886b
RAJ SINGH <103327736+75180rajsingh@users.noreply.github.com>
2022-08-23 09:37:21 +0530

    Create run.c
    code1

---
+++ .C . TUTORIAL.RAJ/run.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d;
+    printf("enter the number a,b,c,d\n");
+    sacnf("%d",&a,b,c,d);
+    printf("the sum ,multiply,division,divide,addition of number a,b,c,d\n");
+    return 0;
+}
